c4270070ab2131c4bf2f6f410777d36b80271614
zZz <57362750+zzzzzzzZzzzzzzZ-pong@users.noreply.github.com>
2022-10-18 16:56:45 +0100

    this is some test

---
+++ bot.py
@@ -0,0 +1,12 @@
+from chatterbot import ChatBot
+import nltk
+
+chatbot = ChatBot('beetle')
+
+exit_conditions = (":q", "quit", "exit")
+while True:
+    query = input("> ")
+    if query in exit_conditions:
+        break
+    else:
+        print(f"beetle: {chatbot.get_response(query)}")
