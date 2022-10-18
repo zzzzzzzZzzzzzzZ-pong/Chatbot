c75e75e8739983e4a04362c596ccbba7ab5bec57
zZz <57362750+zzzzzzzZzzzzzzZ-pong@users.noreply.github.com>
2022-10-18 17:22:14 +0100

    yo

--- bot.py
+++ bot.py
@@ -4,6 +4,20 @@ from chatterbot.trainers import ListTrainer
 chatbot = ChatBot('beetle')
 trainer = ListTrainer(chatbot)
 
+trainer.train([
+    'Hi',
+    'Hello',
+    'Assalaamualaikum',
+    'Welcome',
+    'YOOOOOOOOOOOOOO'
+])
+trainer.train([
+    'this is a bot',
+    'message',
+    'have u ever thought that the short ppl r actually short'
+    'FAST U DUMB GUY'
+])
+
 exit_conditions = (":q", "quit", "exit")
 while True:
     query = input("> ")
